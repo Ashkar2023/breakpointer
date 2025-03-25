a021378da6424503330f372a19f64a4c7d7b132a
ashkar2023 <muhammedashkar2023@gmail.com>
2025-03-26 00:36:06 +0530

    feat: classNames, integrated BreakpointIndicator
    parser update
    removed internal _render prop returned from useBreakpointer

--- site/src/main.tsx
+++ site/src/main.tsx
@@ -7,11 +7,24 @@ import resolveConfig from "tailwindcss/resolveConfig";
 import tailwindConfig from "../tailwind.config"
 
 const config = resolveConfig(tailwindConfig);
-console.log(import.meta.env.MODE);
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BreakpointerProvider breakpointsObj={config.theme.screens} mode={import.meta.env.MODE}>
+        <BreakpointerProvider
+            breakpointsObj={{
+                ...config.theme.screens,
+                dev:"380",
+                ved:400,
+            }}
+            mode={import.meta.env.MODE}
+            classNames={{
+                wrapper: ["bg-gray-300", "text-red-800", "p-4", "rounded-lg", "shadow-lg"],
+                iconWrapper: ["content-start"],
+                screen: ["font-bold", "text-lg", "text-blue-500"],
+                currentWidth: ["text-sm", "text-gray-400"],
+            }}
+        >
+            <p className="text-red-500">sdfsdf</p>
             <App />
         </BreakpointerProvider>
     </StrictMode>,
--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindPlugin from "@tailwindcss/vite"
 
 export default defineConfig({
-    plugins: [react(), tailwindPlugin()],
+    plugins: [react()],
     appType: 'spa',
 })
